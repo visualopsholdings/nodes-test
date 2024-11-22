ca88414197df420bc7f2e8d2b97349d1346c247d
Paul Hamilton <phamtec@mac.com>
2024-11-22 14:55:08 +1100

    Update aggregate.rb

--- features/aggregate.rb
+++ features/aggregate.rb
@@ -1,9 +1,11 @@
 
 def aggregate(script, coll)
 
-   home = ENV['NODES_HOME']
+   # honour env variables for DB name and where NODES is.
    dbname=ENV['MONGO_PORT_27017_DB']
+   home=  ENV['NODES_HOME']
    
+   # same defaults as inside local.sh
    if !dbname
       dbname ='dev'
    end
